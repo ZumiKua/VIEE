c44c490d634a585c14b0944c1bedbe4c36f80172
Zumi Kua <wlzqkwd@Gmail.com>
2025-09-14 12:01:21 +0800

    refactor(Summon Night): Rename constants to UpperCamelCase

--- VieeExtractor/src/Extractors/SLPS02542.cs
+++ VieeExtractor/src/Extractors/SLPS02542.cs
@@ -10,14 +10,14 @@ public class SLPS02542 : IExtractor
 {
     private static readonly Encoding ShiftJISEncoding = Encoding.GetEncoding("shift_jis");
 
-    private const uint BASE_ADDRESS = 0x80097448;
-    private const int LINE_COUNT_OFFSET = 0x760;
-    private const int LINE_ADDRESS_OFFSET = 0x898;
-    private const int MAX_READ = 3;
-    private const int BYTES_PER_READ = 22;
+    private const uint BaseAddress = 0x80097448;
+    private const int LineCountOffset = 0x760;
+    private const int LineAddressOffset = 0x898;
+    private const int MaxRead = 3;
+    private const int BytesPerRead = 22;
     //In case of an illegal value at the line count address causes the program to enter an infinite loop.
-    private const int MAX_VALID_LINE_COUNT = 10;
-    private const int MAP_LOCATION_NAME_COUNT_VALUE = 0x2D;
+    private const int MaxValidLineCount = 10;
+    private const int MapLocationNameCountValue = 0x2D;
 
 
     private readonly ApiContainer _apiContainer;
@@ -25,7 +25,7 @@ public class SLPS02542 : IExtractor
     private readonly StringBuilder _textBuf = new();
     private readonly List<long> _lineAddresses = new();
     private readonly List<long> _lastLineAddresses = new();
-    private readonly byte[] _lineBuf = new byte[MAX_READ * BYTES_PER_READ];
+    private readonly byte[] _lineBuf = new byte[MaxRead * BytesPerRead];
     private bool _fastForwardEmptySent = false;
     private bool _registerNotSupported;
 
@@ -37,7 +37,7 @@ public class SLPS02542 : IExtractor
 
     public void FrameEnd(bool fastForward)
     {
-        ulong gpAddress = BASE_ADDRESS;
+        ulong gpAddress = BaseAddress;
         if (!_registerNotSupported)
         {
             var gpValueNullable = _apiContainer.Emulation.GetRegister("gp");
@@ -56,16 +56,16 @@ public class SLPS02542 : IExtractor
             return;
         }
         var baseAddress = ConvertMemoryAddress(gpAddress);
-        var lineNumber = _apiContainer.Memory.ReadByte(baseAddress + LINE_COUNT_OFFSET);
-        if (lineNumber == MAP_LOCATION_NAME_COUNT_VALUE)
+        var lineNumber = _apiContainer.Memory.ReadByte(baseAddress + LineCountOffset);
+        if (lineNumber == MapLocationNameCountValue)
         {
             lineNumber = 1;
         }
-        if (lineNumber > MAX_VALID_LINE_COUNT)
+        if (lineNumber > MaxValidLineCount)
         {
             return;
         }
-        var lineAddressStart = ConvertMemoryAddress(_apiContainer.Memory.ReadU32(baseAddress + LINE_ADDRESS_OFFSET));
+        var lineAddressStart = ConvertMemoryAddress(_apiContainer.Memory.ReadU32(baseAddress + LineAddressOffset));
         if (lineAddressStart == 0)
         {
             return;
@@ -98,9 +98,9 @@ public class SLPS02542 : IExtractor
         {
             var address = _lineAddresses[i];
             var index = 0;
-            for (var j = 0; j < MAX_READ; j++)
+            for (var j = 0; j < MaxRead; j++)
             {
-                var bytes = _apiContainer.Memory.ReadByteRange(address + j * BYTES_PER_READ, BYTES_PER_READ);
+                var bytes = _apiContainer.Memory.ReadByteRange(address + j * BytesPerRead, BytesPerRead);
                 for (var k = 0; k < bytes.Count; k += 2)
                 {
                     if (bytes[k] == 0 && bytes[k + 1] == 0)
